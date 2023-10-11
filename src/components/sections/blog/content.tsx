54d2296f1e055b6dba571c60179a9c38f2a7017c
Sahisway <sahilswargiary.personal@gmail.com>
2023-10-11 15:07:07 +0530

    Updated responsiveness of contact page

--- src/components/sections/blog/content.tsx
+++ src/components/sections/blog/content.tsx
@@ -19,65 +19,6 @@ function content() {
           <p className="text-para text-white">Designing a design with friendly user experience plays a vital role in the experience a view experiences. follow for guide.</p>
         </div>
       </div>
-
-      {/* ________________________TEST DIV________________________ */}
-      
-      <div className="mt-10 w-1/4 rounded-md bg-slate-900 cursor-pointer">
-        <div>
-          <Img
-            className="w-full overflow-hidden" // Task : Add a hover zoom effect on the image
-            src={Blog_Img}
-            alt="Blog Image"
-          />
-        </div>
-        <div className="p-5">
-          <p className="text-slate-400">Insight : <span>2 Sept 2023</span></p>
-          <h3 className="py-3 text-h3 text-white">Role of UX Designer in making user friendly responsive designs for web apps and websites</h3>
-          <p className="text-para text-white">Designing a design with friendly user experience plays a vital role in the experience a view experiences. follow for guide.</p>
-        </div>
-      </div>
-      <div className="mt-10 w-1/4 rounded-md bg-slate-900 cursor-pointer">
-        <div>
-          <Img
-            className="w-full overflow-hidden" // Task : Add a hover zoom effect on the image
-            src={Blog_Img}
-            alt="Blog Image"
-          />
-        </div>
-        <div className="p-5">
-          <p className="text-slate-400">Insight : <span>2 Sept 2023</span></p>
-          <h3 className="py-3 text-h3 text-white">Role of UX Designer in making user friendly responsive designs for web apps and websites</h3>
-          <p className="text-para text-white">Designing a design with friendly user experience plays a vital role in the experience a view experiences. follow for guide.</p>
-        </div>
-      </div>
-      <div className="mt-10 w-1/4 rounded-md bg-slate-900 cursor-pointer">
-        <div>
-          <Img
-            className="w-full overflow-hidden" // Task : Add a hover zoom effect on the image
-            src={Blog_Img}
-            alt="Blog Image"
-          />
-        </div>
-        <div className="p-5">
-          <p className="text-slate-400">Insight : <span>2 Sept 2023</span></p>
-          <h3 className="py-3 text-h3 text-white">Role of UX Designer in making user friendly responsive designs for web apps and websites</h3>
-          <p className="text-para text-white">Designing a design with friendly user experience plays a vital role in the experience a view experiences. follow for guide.</p>
-        </div>
-      </div>
-      <div className="mt-10 w-1/4 rounded-md bg-slate-900 cursor-pointer">
-        <div>
-          <Img
-            className="w-full overflow-hidden" // Task : Add a hover zoom effect on the image
-            src={Blog_Img}
-            alt="Blog Image"
-          />
-        </div>
-        <div className="p-5">
-          <p className="text-slate-400">Insight : <span>2 Sept 2023</span></p>
-          <h3 className="py-3 text-h3 text-white">Role of UX Designer in making user friendly responsive designs for web apps and websites</h3>
-          <p className="text-para text-white">Designing a design with friendly user experience plays a vital role in the experience a view experiences. follow for guide.</p>
-        </div>
-      </div>
     </div>
   )
 }
