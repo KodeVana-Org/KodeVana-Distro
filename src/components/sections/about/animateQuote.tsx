b2197cdbda86251aadc33e274ea0b3678e74c056
Sahisway <sahilswargiary.personal@gmail.com>
2023-10-13 23:34:04 +0530

    Added about section folder about page

---
+++ src/components/sections/about/animateQuote.tsx
@@ -0,0 +1,14 @@
+
+function AnimateQuote() {
+  return (
+    <div className="mx-2 xs:mx-7 sm:mx-10 md:mx-16 lg:mx-24 xl:mx-40 w-1/3 section-gap">
+      <div className="py-40 relative text-center bg-slate-700 rounded-xl">
+        <h3 className="text-yellow-400 text-h3">Move fast and break things</h3>
+        <p className="pt-3 pb-1 text-para text-white font-semibold">Mark Zuckerberg</p>
+        <p className="text-para text-white">Founder of Facebook</p>
+      </div>
+    </div>
+  )
+}
+
+export default AnimateQuote
